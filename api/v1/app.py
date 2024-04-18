3a2b14095dd44c7bb294308e3f9385e541c905c9
khalilBahi <7753@holbertonstudents.com>
2024-04-18 16:57:07 +0200

    fix import Flask

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,12 +2,12 @@
 """
 API
 """
-import flask
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
 import os
 
-app = flask(__name__)
+app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
