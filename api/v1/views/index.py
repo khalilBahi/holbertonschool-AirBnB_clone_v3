91b47d35ea533fc673d18de715d29b1c0622e746
khalilBahi <7753@holbertonstudents.com>
2024-04-18 17:06:41 +0200

    return the number of counted object

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ Blueprint for the API
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route("/status", methods=["GET"], strict_slashes=False)
@@ -12,3 +13,19 @@ def status():
     Return the status of the API
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", methods=["GET"], strict_slashes=False)
+def stats():
+    """
+    Return the number of counted_objects by type
+    """
+    counted_objects = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+    return jsonify(counted_objects)
