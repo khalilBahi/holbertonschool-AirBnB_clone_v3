fe1e8b983a3f48381bd1b8b5d7b367d2995bd9e0
khalilBahi <7753@holbertonstudents.com>
2024-04-18 16:41:44 +0200

    Status of your API

---
+++ api/v1/views/index.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Blueprint for the API
+"""
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route("/status", methods=["GET"], strict_slashes=False)
+def status():
+    """
+    Return the status of the API
+    """
+    return jsonify({"status": "OK"})
