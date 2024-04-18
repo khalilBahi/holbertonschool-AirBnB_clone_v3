e48cdf1234743c67f59122ee8e889062728637c8
khalilBahi <7753@holbertonstudents.com>
2024-04-18 15:43:43 +0200

    fix err

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -89,13 +89,12 @@ class FileStorage:
                 count += 1
         return count
 
-
     def delete(self, obj=None):
         """delete obj from __objects if it’s inside"""
         if obj is not None:
             key = obj.__class__.__name__ + "." + obj.id
             if key in self.__objects:
-                del self.__objects[key]
+                del self.__objects[k]
 
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
