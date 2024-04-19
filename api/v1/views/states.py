fb8fd22837e955660b4631072dca2201ac48ec3b
khalilBahi <7753@holbertonstudents.com>
2024-04-19 15:41:58 +0200

    Create a new view for State

---
+++ api/v1/views/states.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+"""
+API
+"""
+from flask import jsonify, abort, Request
+from api.v1.views import app_views, storage
+from models.state import State
+
+
+@app_views.route("/states", methods=["GET"], strict_slashes=False)
+def states():
+    """
+    Return: a json representation of all states
+    """
+    states = storage.all("State")
+    states_list = []
+    for state in states.values():
+        states_list.append(state.to_dict())
+
+    return jsonify(states_list)
+
+
+@app_views.route("/states/<state_id>", methods=["GET"], strict_slashes=False)
+def state(state_id):
+    """
+    :return:
+    state_id: string - the state id
+    Return:
+    a json representation of the state
+    """
+
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    return jsonify(state.to_dict())
+
+
+@app_views.route("/states/<state_id>",methods=["DELETE"], strict_slashes=False)
+def delete_state(state_id):
+    """
+    :return:
+    state_id: string - the state id
+    Return:
+    a json representation of the state
+    """
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+
+    storage.delete(state)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route("/states", methods=["POST"], strict_slashes=False)
+def create_state():
+    """
+    :return:
+    Return:
+    a json representation of the state
+    """
+    data=Request.get_json(force=False, silent=True, cache=False)
+    if data is None:
+        abort("Not a JSON"),400
+    if "name" not in data:
+        abort(400, "Missing name")
+    state = State(**data)
+    state.save()
+    return jsonify(state.to_dict()), 201
+
+
+@app_views.route("/states/<state_id>", methods=["PUT"], strict_slashes=False)
+def update_state(state_id):
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    data=Request.get_json(force=False, silent=True, cache=False)
+    if data is None:
+        abort(400, "Not a JSON")
+    for key, value in data.items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(state, key, value)
+    state.save()
+    return jsonify(state.to_dict()), 200
+    
